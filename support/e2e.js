f06ac0fa5503ece76f044f139588b3fb150e900b
Laurent <aurel900104@gmail.com>
2023-03-18 14:19:33 +0100

    Initial commit

---
+++ support/e2e.js
@@ -0,0 +1,34 @@
+// ***********************************************************
+// This example support/e2e.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    setupNodeEvents(on, config) {
+      // bind to the event we care about
+      on('<event>', (arg1, arg2) => {
+        // plugin stuff here
+      })
+    },
+  },
+})
